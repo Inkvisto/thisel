c94f9883ccdaafdcae473a19080e9c205b7f291a
Inkvisto <qwertuasdfgh953@gmail.com>
2024-04-12 10:13:15 +0300

    Initial commit

---
+++ src/regex.rs
@@ -0,0 +1,9 @@
+#[macro_export]
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        use {regex::Regex, std::sync::OnceLock};
+
+        static RE: OnceLock<Regex> = OnceLock::new();
+        RE.get_or_init(|| Regex::new($re).unwrap())
+    }};
+}
